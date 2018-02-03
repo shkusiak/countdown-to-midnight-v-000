b47e895c7837333a00179bf4126210108d8adac0
Shea Kusiak <shkusiak@gmail.com>
2018-02-03 23:48:15 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,9 @@
 #write your code here
 
 def countdown(number)
-#  while number >= 0 do
-#    puts "#{number} SECOND(S)!"
+  while number >= 0 do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
 
-#  end
-#
 end
